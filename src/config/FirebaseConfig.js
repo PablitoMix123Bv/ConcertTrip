398dfcf814039a506da72b0a241f1d4cb36ce830
IsaacMedina7 <isaaccisnerosmedina44@gmail.com>
2026-05-06 00:00:26 -0600

    Creacion de las Pantallas: WelcomeScreen, RegisterScreen, SplashScreen y LoginScreen, se modifico el theme con la nueva paleta de colores, se creo un nuevo componente demoninado input y se documento en el README los cambios implementados

--- src/config/FirebaseConfig.js
+++ src/config/FirebaseConfig.js
@@ -1,11 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {initializeAuth, getReactNativePersistence} from 'firebase/auth';
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDDVWYJdeRCNGcVfY3nOzDLAlJcy9YSIEY",
   authDomain: "concerttrip-403d5.firebaseapp.com",
@@ -15,8 +12,8 @@ const firebaseConfig = {
   appId: "1:673728410022:web:2537439f609b3a966e5f67"
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = initializeAuth(app, {
-    persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
 });
+export const db = getFirestore(app);  
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,18 +1,19 @@
-import React, {createContext, useState, useEffect, use } from 'react';
+import React, { createContext, useState, useEffect, use } from 'react';
 import { auth, db } from "../config/FirebaseConfig"
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { doc, setDoc } from 'firebase/firestore';
 import { signInWithEmailAndPassword } from "../config/FirebaseConfig";
-import {signOut} from 'firebase/auth';
+import { signOut } from 'firebase/auth';
 import { validateLoginData } from '../utils/authValidation';
 
 // Proveedor de autenticación/ Servicio que gestiona la identidad e inicio de sesión de los usuarios
+export const AuthContext = createContext();
 
-export const AuthProvider = ({children}) => {
+export const AuthProvider = ({ children }) => {
 
-    const [user, setUser] = useState(null); //Almacenamos al usuari
+    const [user, setUser] = useState(null); //Almacenamos al usuario
     const [loading, setLoading] = useState(true); //Verificación de la sesión.
-    
+
     const login = async (email, password) => {
         const validation = validateLoginData(email, password);
 
@@ -25,29 +26,29 @@ export const AuthProvider = ({children}) => {
             // Validación de las credenciales
             await signInWithEmailAndPassword(auth, email, password);
         } catch (error) {
-            console.error ("Error en login:" , error.code);
+            console.error("Error en login:", error.code);
             throw error; //Lanzamos el error para mostrar una alerta dentro del dispositivo
         }
     };
 
-    const signUp = async ( userData ) => {
-      const { email, password, nombre, ciudad, rol } = userData;
+    const signUp = async (userData) => {
+        const { email, password, nombre, ciudad, rol } = userData;
 
         try {
             // Paso 1. Creación del usuario en Auth
-            const userCredential = await createUserWithEmailAndPasswordf(auth, email, password);
+            const userCredential = await createUserWithEmailAndPassword(auth, email, password);
             const uid = userCredential.user.uid;
 
             // Paso 2: Crear el perfil del Firestore usando el misomo UID
-            await setDoc(doc(db, "usuarios", uid),{
-                nombre: nombre, 
+            await setDoc(doc(db, "usuarios", uid), {
+                nombre: nombre,
                 ciudad: ciudad,
                 rol: rol,
-                createdAt: new Date()  
+                createdAt: new Date()
             });
         } catch (error) {
             // Eliminicion del usuario registrado en Auth
-            if(userCredential?.user){
+            if (userCredential?.user) {
                 await userCredential.user.delete();
                 console.log("Limpieza exitosa: Usuario eliminado de Auth por fallo en la base de datos")
             }
@@ -64,7 +65,7 @@ export const AuthProvider = ({children}) => {
         }
     }
 
-    useEffect (() => {
+    useEffect(() => {
         // onAuthStateChanged se activa automáticamente cada vez que alguien o sale de la app.
         // Monitorea de forma asincrona el token de sesión del usuario.
         const unsubscribe = auth.onAuthStateChanged((userFirebase) => {
@@ -75,10 +76,8 @@ export const AuthProvider = ({children}) => {
     }, []);
 
     return (
-        <AuthContext.Provider value = {{ user, loading, login, signUp, logout }}>
+        <AuthContext.Provider value={{ user, loading, login, signUp, logout }}>
             {children}
         </AuthContext.Provider>
     );
 };
-
-export const AuthContext = createContext();
