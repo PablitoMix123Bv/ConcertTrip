7d49daf3fceb2eef9727f10c59fd1d816930a098
PablitoMixBv <pablomor465@gmail.com>
2026-05-07 12:49:32 -0600

    Arreglo en la interfaz gráfica (colores)

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,4 +1,4 @@
-import {COLORS, SIZES} from "../constants/theme";
+import {COLORS, COLORSCREEN, SIZES} from "../constants/theme";
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 import { useState } from "react";
@@ -11,17 +11,17 @@ const NavigationMenu = () => {
     }   
     return(
         <View style = {styles.menuContainer}>
-            <TouchableOpacity onPress = {() => handleActiveTab('home')} style = {[styles.touchableStyles,{backgroundColor: activeTab === 'home' ? COLORS.borders : '#ececec',}]}>
-                <Ionicons name = "home" size = {SIZES.MenuIconSize} color = {activeTab === 'home' ? COLORS.primary : '#9e9e9e'}/>
+            <TouchableOpacity onPress = {() => handleActiveTab('home')} style = {[styles.touchableStyles,{backgroundColor: activeTab === 'home' ? COLORS.borders : COLORSCREEN.background,}]}>
+                <Ionicons name = "home" size = {SIZES.MenuIconSize} color = {activeTab === 'home' ? COLORS.primaryDark : COLORS.secondary}/>
             </TouchableOpacity>
-            <TouchableOpacity onPress = {() => handleActiveTab('search')} style = {[styles.touchableStyles, {backgroundColor: activeTab === 'search' ? COLORS.borders : '#ececec'}]}>
-                <Ionicons name = "search" size = {SIZES.MenuIconSize} color = {activeTab === 'search' ? COLORS.primary : '#9e9e9e'}/>
+            <TouchableOpacity onPress = {() => handleActiveTab('search')} style = {[styles.touchableStyles, {backgroundColor: activeTab === 'search' ? COLORS.borders : COLORSCREEN.background}]}>
+                <Ionicons name = "search" size = {SIZES.MenuIconSize} color = {activeTab === 'search' ? COLORS.primaryDark : COLORS.secondary}/>
             </TouchableOpacity>
-            <TouchableOpacity onPress = {() => handleActiveTab('ticket')} style = {[styles.touchableStyles, {backgroundColor: activeTab === 'ticket' ? COLORS.borders : '#ececec'}]}>
-                <Ionicons name = "ticket" size = {SIZES.MenuIconSize} color = {activeTab === 'ticket' ? COLORS.primary : '#9e9e9e'}/>
+            <TouchableOpacity onPress = {() => handleActiveTab('ticket')} style = {[styles.touchableStyles, {backgroundColor: activeTab === 'ticket' ? COLORS.borders : COLORSCREEN.background}]}>
+                <Ionicons name = "ticket" size = {SIZES.MenuIconSize} color = {activeTab === 'ticket' ? COLORS.primaryDark : COLORS.secondary}/>
             </TouchableOpacity>
-            <TouchableOpacity onPress = {() => handleActiveTab('person')} style = {[styles.touchableStyles, {backgroundColor: activeTab === 'person' ? COLORS.borders : '#ececec'}]}>
-                <Ionicons name = "person" size = {SIZES.MenuIconSize} color = {activeTab === 'person' ? COLORS.primary : '#9e9e9e'}/>
+            <TouchableOpacity onPress = {() => handleActiveTab('person')} style = {[styles.touchableStyles, {backgroundColor: activeTab === 'person' ? COLORS.borders : COLORSCREEN.background}]}>
+                <Ionicons name = "person" size = {SIZES.MenuIconSize} color = {activeTab === 'person' ? COLORS.primaryDark : COLORS.secondary}/>
             </TouchableOpacity>
         </View>
     );
@@ -37,7 +37,9 @@ const styles = StyleSheet.create({
         alignContent: "space-around",
         position: "absolute",
         bottom: 0,
-        backgroundColor: "#ececec"
+        backgroundColor: COLORS.background,
+        borderTopColor: COLORS.primary,
+        borderWidth: 0.5,
     },
     touchableStyles: {
         padding: 12,
