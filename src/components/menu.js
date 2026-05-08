48940b8ab452778d027fafe5497b82628c54c9a8
IsaacMedina7 <isaaccisnerosmedina44@gmail.com>
2026-05-07 23:27:51 -0600

    chore: actualizar menu.js

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import { COLORS } from "../constants/theme";
-=======
-import {COLORS, SIZES} from "../constants/theme";
->>>>>>> origin/main
+import { COLORS, SIZES } from "../constants/theme";
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 import { useState } from "react";
@@ -12,37 +8,20 @@ const NavigationMenu = () => {
 
     const handleActiveTab = (TabName) => {
         setActiveTab(TabName)
-<<<<<<< HEAD
     }
     return (
         <View style={styles.menuContainer}>
-            <TouchableOpacity onPress={() => handleActiveTab('home')} style={{ paddingVertical: 15, paddingHorizontal: 20, backgroundColor: activeTab === 'home' ? COLORS.background : '#ececec', borderRadius: 20 }}>
-                <Ionicons name="home" size={24} color={activeTab === 'home' ? COLORS.primary : '#9e9e9e'} />
+            <TouchableOpacity onPress={() => handleActiveTab('home')} style={[styles.touchableStyles, { backgroundColor: activeTab === 'home' ? COLORS.secondary : COLORS.background, }]}>
+                <Ionicons name="home" size={SIZES.MenuIconSize} color={activeTab === 'home' ? COLORS.primaryDark : COLORS.secondary} />
             </TouchableOpacity>
-            <TouchableOpacity onPress={() => handleActiveTab('search')} style={{ paddingVertical: 15, paddingHorizontal: 20, backgroundColor: activeTab === 'search' ? COLORS.background : '#ececec', borderRadius: 20 }}>
-                <Ionicons name="search" size={24} color={activeTab === 'search' ? COLORS.primary : '#9e9e9e'} />
+            <TouchableOpacity onPress={() => handleActiveTab('search')} style={[styles.touchableStyles, { backgroundColor: activeTab === 'search' ? COLORS.secondary : COLORS.background }]}>
+                <Ionicons name="search" size={SIZES.MenuIconSize} color={activeTab === 'search' ? COLORS.primaryDark : COLORS.secondary} />
             </TouchableOpacity>
-            <TouchableOpacity onPress={() => handleActiveTab('ticket')} style={{ paddingVertical: 15, paddingHorizontal: 20, backgroundColor: activeTab === 'ticket' ? COLORS.background : '#ececec', borderRadius: 20 }}>
-                <Ionicons name="ticket" size={24} color={activeTab === 'ticket' ? COLORS.primary : '#9e9e9e'} />
+            <TouchableOpacity onPress={() => handleActiveTab('ticket')} style={[styles.touchableStyles, { backgroundColor: activeTab === 'ticket' ? COLORS.secondary : COLORS.background }]}>
+                <Ionicons name="ticket" size={SIZES.MenuIconSize} color={activeTab === 'ticket' ? COLORS.primaryDark : COLORS.secondary} />
             </TouchableOpacity>
-            <TouchableOpacity onPress={() => handleActiveTab('person')} style={{ paddingVertical: 15, paddingHorizontal: 20, backgroundColor: activeTab === 'person' ? COLORS.background : '#ececec', borderRadius: 20 }}>
-                <Ionicons name="person" size={24} color={activeTab === 'person' ? COLORS.primary : '#9e9e9e'} />
-=======
-    }   
-    return(
-        <View style = {styles.menuContainer}>
-            <TouchableOpacity onPress = {() => handleActiveTab('home')} style = {[styles.touchableStyles,{backgroundColor: activeTab === 'home' ? COLORS.secondary : COLORS.background,}]}>
-                <Ionicons name = "home" size = {SIZES.MenuIconSize} color = {activeTab === 'home' ? COLORS.primaryDark : COLORS.secondary}/>
-            </TouchableOpacity>
-            <TouchableOpacity onPress = {() => handleActiveTab('search')} style = {[styles.touchableStyles, {backgroundColor: activeTab === 'search' ? COLORS.secondary : COLORS.background}]}>
-                <Ionicons name = "search" size = {SIZES.MenuIconSize} color = {activeTab === 'search' ? COLORS.primaryDark : COLORS.secondary}/>
-            </TouchableOpacity>
-            <TouchableOpacity onPress = {() => handleActiveTab('ticket')} style = {[styles.touchableStyles, {backgroundColor: activeTab === 'ticket' ? COLORS.secondary : COLORS.background}]}>
-                <Ionicons name = "ticket" size = {SIZES.MenuIconSize} color = {activeTab === 'ticket' ? COLORS.primaryDark : COLORS.secondary}/>
-            </TouchableOpacity>
-            <TouchableOpacity onPress = {() => handleActiveTab('person')} style = {[styles.touchableStyles, {backgroundColor: activeTab === 'person' ? COLORS.secondary : COLORS.background}]}>
-                <Ionicons name = "person" size = {SIZES.MenuIconSize} color = {activeTab === 'person' ? COLORS.primaryDark : COLORS.secondary}/>
->>>>>>> origin/main
+            <TouchableOpacity onPress={() => handleActiveTab('person')} style={[styles.touchableStyles, { backgroundColor: activeTab === 'person' ? COLORS.secondary : COLORS.background }]}>
+                <Ionicons name="person" size={SIZES.MenuIconSize} color={activeTab === 'person' ? COLORS.primaryDark : COLORS.secondary} />
             </TouchableOpacity>
         </View>
     );
